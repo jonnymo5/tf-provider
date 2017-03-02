54e2532eceda7d9032807a57a83adc3e61e99702
JonnyMo5 <jonnymo5@gmail.com>
2017-03-02 09:50:33 -0600

    fixed issues with blog code

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
   "github.com/hashicorp/terraform/plugin"
+  "github.com/hashicorp/terraform/terraform"
 )
 
 func main()  {
   plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: func () terraform.ResourceProvider {
+    ProviderFunc: func() terraform.ResourceProvider {
       return Provider()
-    }
+    },
   })
 }
--- provider.go
+++ provider.go
@@ -6,6 +6,6 @@ import (
 
 func Provider() *schema.Provider {
   return &schema.Provider{
-    ResourcesMap: map[string]*schema.Resource(),
+    ResourcesMap: map[string]*schema.Resource{},
   }
 }
